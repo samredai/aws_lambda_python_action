39aa56a40478188aec3347f6fc86d99910264202
samsetegne <samuelsetegne@gmail.com>
2019-10-04 23:00:13 -0400

    Adding Dockerfile and entrypoint script

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.7
+
+LABEL "com.github.actions.name"="Deploy Python Function to AWS Lambda"
+LABEL "com.github.actions.description"="Deploy python code to AWS Lambda with dependencies in a separate layer."
+LABEL "com.github.actions.icon"="layers"
+LABEL "com.github.actions.color"="yellow"
+
+RUN apt-get update
+RUN apt-get install -y jq zip
+RUN pip install awscli
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+install_zip_dependencies(){
+	echo "Installing and zipping dependencies..."
+	mkdir python
+	pip install --target=python $1
+	zip -r dependencies.zip ./python
+}
+
+publish_dependencies_as_layer(){
+	echo "Publishing dependencies as a layer..."
+	local result=$(aws lambda publish-layer-version --layer-name "${LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
+	LAYER_VERSION=$(jq '.Version' <<< "$result")
+	rm -rf python
+	rm dependencies.zip
+}
+
+publish_function_code(){
+	echo "Deploying the code itself..."
+	zip -r code.zip . -x \*.git\*
+	aws lambda update-function-code --function-name "${LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+}
+
+update_function_layers(){
+	echo "Using the layer in the function..."
+	aws lambda update-function-configuration --function-name "${LAMBDA_FUNCTION_NAME}" --layers "${LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+}
+
+deploy_lambda_function(){
+	install_zip_dependencies
+	publish_dependencies_as_layer
+	publish_function_code
+	update_function_layers
+}
+
+deploy_lambda_function
+echo "Done."
