f642226d1d2d83a78fc712c531fe2dd534467454
Samuel L. Setegne <43911210+samsetegne@users.noreply.github.com>
2019-11-16 14:20:41 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,8 @@ update_function_layers(){
 }
 
 deploy_lambda_function(){
-	install_zip_dependencies $1
+        requirements = "$*"
+	install_zip_dependencies $requirements
 	publish_dependencies_as_layer
 	publish_function_code
 	update_function_layers
