41def0f8e06de64526e25ef201e1f6704b4f5d08
Samuel L. Setegne <43911210+samsetegne@users.noreply.github.com>
2019-11-16 17:53:40 -0800

    Removing $requirements, and concatenating args

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,12 +27,11 @@ update_function_layers(){
 }
 
 deploy_lambda_function(){
-        requirements="$*"
-	install_zip_dependencies "$requirements"
+	install_zip_dependencies $1
 	publish_dependencies_as_layer
 	publish_function_code
 	update_function_layers
 }
 
-deploy_lambda_function $1
+deploy_lambda_function "$*"
 echo "Done."
